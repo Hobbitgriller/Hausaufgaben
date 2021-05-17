e565550a9a6428c0e1223e892c672ddf1ba7c68d
Max Philipp Holl <maxp.holl@gmx.de>
2021-05-17 11:43:20 +0200

    Mit Korrekturen zurueck

--- Hausaufgabe3.py
+++ Hausaufgabe3.py
@@ -22,10 +22,12 @@ Matrix = np.array([[0,1,1],[1,0,1],[1,1,0]])
 AdMatrix = 50*Matrix
 Frequenz = np.array([0.19,0.2,0.21])
 
-Zeitschritt = 0.05
-Zeit = np.arange(0,100,Zeitschritt)
+Zeitschritt = 0.001	# dein Zeitschritt war zu groß
+Zeit = np.arange(0,10,Zeitschritt)
 
 xList = np.zeros([Zeit.size,Anzahl])
+xList[0] = np.random.uniform(0., 2*np.pi, Anzahl)	# random Initial Conditions
+print(xList[0])						# check ob ich scheiß gebaut hab
 k1 = np.zeros(Anzahl)
 k2 = np.zeros(Anzahl)
 k3 = np.zeros(Anzahl)
@@ -46,7 +48,8 @@ def Funktion(xWerte, Alphas, AdjMatrix):
     x_Ableitung = FunktionList
     return x_Ableitung
 
-for i in range(Zeit.size):
+for i in range(1, Zeit.size):	# du musst bei 1 anfangen, da der 0. Schritt ja nicht erst ausgerechnet mist. [i-1] waere dann [-1] also der letzte Schritt, also immer 0.
+#				  Deswegen war deinem Code die Anfangsbedingung egal. 
     
     k1 = Funktion(xList[i-1],Frequenz,AdMatrix)
     k2 = Funktion(xList[i-1]+Zeitschritt/2*k1,Frequenz,AdMatrix)
@@ -54,6 +57,7 @@ for i in range(Zeit.size):
     k4 = Funktion(xList[i-1]+Zeitschritt*k3,Frequenz,AdMatrix)
     xList[i] = xList[i-1]+ Zeitschritt/6*(k1+(2*k2)+(2*k3)+k4) 
     
+print(xList) 	# check obs jetzt funktioniert
 Ergebnisx1 = np.zeros(Zeit.size)
 Ergebnisx2 = np.zeros(Zeit.size)
 Ergebnisx3 = np.zeros(Zeit.size)
@@ -68,5 +72,5 @@ plt.plot(Zeit, (Ergebnisx2),label="Oszi 2", color="blue")
 plt.plot(Zeit, (Ergebnisx3),label="Oszi 3", color="green")
 plt.legend()
 
-
-
+plt.savefig('smallstep.pdf') # ich brauchte ein Beispiel für heute Nachmittag. 
+plt.show()
